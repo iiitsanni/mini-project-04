0cb362e855dfc96df5dc28dd042088ae41fd439b
iiitsanni <annikamaringer@online.de>
2026-02-26 17:36:41 -0500

    changing the pagination layout

--- mini-project-04/src/components/pagination.jsx
+++ mini-project-04/src/components/pagination.jsx
@@ -6,11 +6,11 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
 
   return (
     <div className="flex justify-center mt-8 mb-8 ">
-      <div className="join ">
+      <div className="join space-x-2">
         {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
           <button
             key={page}
-            className={`join-item btn bg-[#77ACA2] text-[#031926] border-[#031926] shadow-none rounded-[1.5em] m-1 ${currentPage === page ? 'btn-active ' : ''}`}
+            className={`join-item btn bg-[#77ACA2] text-[#031926] border-[#031926] shadow-none rounded-[1.5em] ${currentPage === page ? 'btn-active ' : ''}`}
             onClick={() => onPageChange(page)}
           >
             {page}
