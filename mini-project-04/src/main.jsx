6a45ad59425ad08705a493cf70a1ec75df1e53a9
iiitsanni <annikamaringer@online.de>
2026-02-26 19:51:53 -0500

    fixes to deploy website

--- mini-project-04/src/main.jsx
+++ mini-project-04/src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import './components/navbar.css'
-import './components/footer.css'
+import './components/Navbar.css'
+import './components/Footer.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
